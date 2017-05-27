342d4ccc4a5f67fd622422d5b10f470eebefe706
bagf <rory@eishgaming.co.za>
2017-05-27 18:27:02 +0200

    Fixed test

--- tests/BasicTimeStringificationTest.php
+++ tests/BasicTimeStringificationTest.php
@@ -10,6 +10,6 @@ class BasicTimeStringificationTest extends TestCase
         $this->assertEquals('12 secs', Juration::stringify(12));
         $this->assertEquals('3 mins 4 secs', Juration::stringify(184));
         $this->assertEquals('2 hrs 20 mins', Juration::stringify(8400));
-        $this->assertEquals('6 mos 1 day', Juration::stringify(15854400));
+        $this->assertEquals('6 mths 1 day', Juration::stringify(15854400));
     }
 }
